6e736933685567af37c9f3cd8a0a8f86b874ed0a
Mattisv <mattis.vainikainen@gmail.com>
2019-09-06 07:21:21 +0200

    added topshooter

---
+++ Going Down/Assets/RobotController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody2D _rb;
+
+    private void Start() {
+    _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
